c049b09758c667e76c34160db6c6d48e27ae0c19
cgradwohl <15326707+cgradwohl@users.noreply.github.com>
2021-09-06 11:05:47 -0700

    save.

--- src/ingestion-worker.ts
+++ src/ingestion-worker.ts
@@ -1,15 +1,25 @@
 import { KinesisStreamEvent, KinesisStreamRecord } from "aws-lambda";
-import * as SQS from "./lib/sqs";
+import kinesisToJson from "./lib/kinesis-to-json";
+import { enqueue } from "./lib/sqs";
 
-const enqueueJob = SQS.enqueue<any>(process.env.JOB_QUEUE);
+interface Job {
+  action: string;
+  user: {
+    id: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+  };
+}
+
+const enqueueJobWorker = enqueue<Job>(process.env.JOB_QUEUE);
 
 // for each kinesis record in the batch, put record into sqs
 const processBatch = async (record: KinesisStreamRecord) => {
-  console.log("::: RECORD ::", JSON.stringify(record, null, 2));
-  await enqueueJob(record);
+  const item = kinesisToJson<any>(record?.kinesis?.data);
+  enqueueJobWorker(item);
 };
 
 export default async (event: KinesisStreamEvent) => {
-  console.log("event.Records.length", event.Records.length);
   await Promise.all(event.Records.map(processBatch));
 };
--- src/job-worker.ts
+++ src/job-worker.ts
@@ -1,9 +1,17 @@
 import { SQSEvent, SQSRecord } from "aws-lambda";
+import { getRandomIntFromRange } from "./lib/getRandomIntFromRange";
+import { work } from "./lib/work";
 
 const processEvent = async (record: SQSRecord) => {
-  console.log("::: RECORD :::", record);
+  console.log("::: SQS RECORD :::", record);
 
   // switch on each action type
+
+  const workInterval = getRandomIntFromRange(100, 2000);
+
+  console.log(workInterval);
+
+  await work(workInterval);
 };
 
 export default async (event: SQSEvent) => {
